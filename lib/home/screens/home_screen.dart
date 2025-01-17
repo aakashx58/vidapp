a01dfd831a7d69f662ea96d962ef2dd18ad595ba
aakashx58 <aakashrajbanshi58@gmail.com>
2025-01-17 14:55:11 +0545

    fix:Reinitialize the video player when the app is resumed

--- lib/home/screens/home_screen.dart
+++ lib/home/screens/home_screen.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:video_feed/common/utils/bottom_sheet_utils.dart';
 import 'package:video_feed/components/utils/fileshare_utils.dart';
-import 'package:video_feed/features/comment/bloc/cubit/comments_cubit.dart';
 import 'package:video_feed/styles/app_colors.dart';
 import 'package:video_feed/home/screens/profile_screen.dart';
 import 'package:video_feed/common/constants/string_constants.dart';
 import 'package:video_player/video_player.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with WidgetsBindingObserver {
   final PageController _pageController = PageController();
   VideoPlayerController? _currentController;
   int _currentIndex = 0;
 
-  // List of video URLs
-  final List<String> videoUrls = VideoUrls.urls;
+  List<String> videoUrls = VideoUrls.urls;
 
   final List<Widget> _screens = [
     const Center(
@@ -38,17 +35,21 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addObserver(this);
     _initializeAndPlay(0);
   }
 
   @override
   void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
     _currentController?.dispose();
     super.dispose();
   }
 
+  /// Initialize and play the video at the given index
   void _initializeAndPlay(int index) {
     _currentController?.dispose();
+
     _currentController = VideoPlayerController.asset(videoUrls[index])
       ..initialize().then((_) {
         if (mounted) {
@@ -61,12 +62,14 @@ class _HomeScreenState extends State<HomeScreen> {
       });
   }
 
+  /// Pause the current video
   void _pauseCurrentVideo() {
-    if (_currentController?.value.isPlaying ?? false) {
+    if (_currentController != null && _currentController!.value.isPlaying) {
       _currentController?.pause();
     }
   }
 
+  /// Toggle play/pause on video tap
   void _togglePlayPause() {
     if (_currentController != null) {
       setState(() {
@@ -79,9 +82,10 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  /// Handle bottom navigation item tap
   void _onTap(int index) {
     if (_currentIndex != index) {
-      _pauseCurrentVideo();
+      _pauseCurrentVideo(); // Pause the video when switching tabs
       setState(() {
         _currentIndex = index;
       });
@@ -89,111 +93,121 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      // Reinitialize the video player when the app is resumed
+      _initializeAndPlay(_currentIndex);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => CommentsCubit(),
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: _currentIndex == 0
-            ? PageView.builder(
-                controller: _pageController,
-                itemCount: videoUrls.length,
-                scrollDirection: Axis.vertical,
-                onPageChanged: (index) {
-                  _pauseCurrentVideo();
-                  _initializeAndPlay(index);
-                },
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: _togglePlayPause,
-                    child: Stack(
-                      fit: StackFit.expand,
-                      children: [
-                        _currentController != null &&
-                                _currentController!.value.isInitialized
-                            ? FittedBox(
-                                fit: BoxFit.cover,
-                                child: SizedBox(
-                                  width: _currentController!.value.size.width,
-                                  height: _currentController!.value.size.height,
-                                  child: VideoPlayer(_currentController!),
-                                ),
-                              )
-                            : const Center(child: CircularProgressIndicator()),
-                        if (_currentController != null &&
-                            !_currentController!.value.isPlaying)
-                          const Center(
-                            child: Icon(
-                              Icons.play_arrow,
-                              size: 80,
-                              color: Colors.white,
-                            ),
-                          ),
-                        Positioned(
-                          bottom: 20,
-                          right: 20,
-                          child: Column(
-                            children: [
-                              IconButton(
-                                icon: const Icon(Icons.comment,
-                                    color: Colors.white, size: 30),
-                                onPressed: () {
-                                  BottomSheetUtils.showCommentsSheet(
-                                    context: context,
-                                    videoId: videoUrls[index],
-                                  );
-                                },
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: _currentIndex == 0
+          ? PageView.builder(
+              controller: _pageController,
+              itemCount: videoUrls.length,
+              scrollDirection: Axis.vertical,
+              onPageChanged: (index) {
+                _pauseCurrentVideo();
+                _initializeAndPlay(index);
+              },
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: _togglePlayPause, // Toggle play/pause on tap
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      // Display the video
+                      _currentController != null &&
+                              _currentController!.value.isInitialized
+                          ? FittedBox(
+                              fit: BoxFit.cover,
+                              child: SizedBox(
+                                width: _currentController!.value.size.width,
+                                height: _currentController!.value.size.height,
+                                child: VideoPlayer(_currentController!),
                               ),
-                              const SizedBox(height: 10),
-                              IconButton(
-                                icon: const Icon(Icons.share,
-                                    color: Colors.white, size: 30),
-                                onPressed: () async {
-                                  try {
-                                    await FileSharingUtil.shareLocalFile(
-                                        videoUrls[index]);
-                                  } catch (e) {
-                                    debugPrint('Sharing failed: $e');
-                                  }
-                                },
-                              ),
-                            ],
+                            )
+                          : const Center(child: CircularProgressIndicator()),
+
+                      // Show play icon when video is paused
+                      if (_currentController != null &&
+                          !_currentController!.value.isPlaying)
+                        const Center(
+                          child: Icon(
+                            Icons.play_arrow,
+                            size: 80,
+                            color: Colors.white,
                           ),
                         ),
-                      ],
-                    ),
-                  );
-                },
-              )
-            : _screens[_currentIndex],
-        bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: AppColors.primary,
-          unselectedItemColor: AppColors.shadow,
-          currentIndex: _currentIndex,
-          onTap: _onTap,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.group),
-              label: "Friends",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: "Add",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              label: "Chat",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-            ),
-          ],
-        ),
+
+                      // Overlay UI for actions
+                      Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: Column(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.comment,
+                                  color: Colors.white, size: 30),
+                              onPressed: () {
+                                BottomSheetUtils.showCommentsSheet(
+                                  context: context,
+                                  videoId: videoUrls[index],
+                                );
+                              },
+                            ),
+                            const SizedBox(height: 10),
+                            IconButton(
+                              icon: const Icon(Icons.share,
+                                  color: Colors.white, size: 30),
+                              onPressed: () async {
+                                try {
+                                  await FileSharingUtil.shareLocalFile(
+                                      videoUrls[index]);
+                                } catch (e) {
+                                  debugPrint('Sharing failed: $e');
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            )
+          : _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: AppColors.shadow,
+        currentIndex: _currentIndex,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "Add",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Notifications",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
       ),
     );
   }
